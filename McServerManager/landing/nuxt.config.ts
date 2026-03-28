7d24254ef15c616eddc612821c450a240ada64d9
mionsyun <syun13812@gmail.com>
2026-03-28 09:20:23 +0900

    chore: update nuxt dependency from 3.16.0 to 3.21.2

--- McServerManager/landing/nuxt.config.ts
+++ McServerManager/landing/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV === "development" },
   css: ["~/assets/css/main.css"],
 
   runtimeConfig: {
